ed3a4eb469440f22a1b33f2c095e06ac607c373e
Wren Powell <wrentpowell@gmail.com>
2019-11-26 20:16:56 +0000

    Add SQL for creating database tables

---
+++ migrations/2019-11-26-194750_create_tables/down.sql
@@ -0,0 +1,7 @@
+DROP TABLE File;
+DROP TABLE RegularFile;
+DROP TABLE SymbolicLink;
+DROP TABLE Directory;
+DROP TABLE ExtendedAttribute;
+DROP TABLE Blob;
+DROP TABLE Block;
---
+++ src/schema.rs
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2019 Wren Powell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+table! {
+    Blob (id) {
+        id -> Nullable<Integer>,
+        size -> Integer,
+        checksum -> Binary,
+        data -> Binary,
+    }
+}
+
+table! {
+    Block (file_id, blob_id, index) {
+        file_id -> Nullable<Integer>,
+        blob_id -> Nullable<Integer>,
+        index -> Nullable<Integer>,
+    }
+}
+
+table! {
+    Directory (file_id) {
+        file_id -> Nullable<Integer>,
+    }
+}
+
+table! {
+    ExtendedAttribute (id) {
+        id -> Nullable<Integer>,
+        file_id -> Integer,
+        name -> Text,
+        value -> Binary,
+    }
+}
+
+table! {
+    File (id) {
+        id -> Nullable<Integer>,
+        path -> Text,
+        modifiedTime -> Integer,
+        permissions -> Nullable<Integer>,
+    }
+}
+
+table! {
+    RegularFile (file_id) {
+        file_id -> Nullable<Integer>,
+        size -> Integer,
+        checksum -> Binary,
+    }
+}
+
+table! {
+    SymbolicLink (file_id) {
+        file_id -> Nullable<Integer>,
+        target -> Text,
+    }
+}
+
+joinable!(Block -> Blob (blob_id));
+joinable!(Block -> File (file_id));
+joinable!(Directory -> File (file_id));
+joinable!(ExtendedAttribute -> File (file_id));
+joinable!(RegularFile -> File (file_id));
+joinable!(SymbolicLink -> File (file_id));
+
+allow_tables_to_appear_in_same_query!(
+    Blob,
+    Block,
+    Directory,
+    ExtendedAttribute,
+    File,
+    RegularFile,
+    SymbolicLink,
+);
