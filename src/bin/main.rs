34378ac1b268b40067ed6445a523655bb716de96
Garrett Powell <garrett@gpowell.net>
2019-12-02 20:57:42 +0000

    Fix bug writing to files that are opened read-only

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -14,18 +14,45 @@
  * limitations under the License.
  */
 
+use std::io::Read;
 use std::path::PathBuf;
 
-use disk_archive::{Archive, ArchiveEntry};
+use disk_archive::{Archive, ArchiveEntry, Result};
 
 fn main() {
+    // create().unwrap();
+    read().unwrap();
+}
+
+fn read() -> Result<()> {
     let path = PathBuf::from("/home/garrett/test-archive");
-    let mut archive = Archive::create(&path).unwrap();
+    let archive = Archive::open(&path)?;
+
+    let entry = archive.get("null").unwrap();
+    let handle = &entry.data.as_ref().unwrap();
+    println!("{:?}", entry.metadata);
+    println!("{:?}", handle);
+
+    let mut buffer = Vec::new();
+    let mut reader = archive.read(handle)?;
+    reader.read_to_end(&mut buffer)?;
+
+    println!("{:?}", &buffer);
+
+    Ok(())
+}
+
+fn create() -> Result<()> {
+    let path = PathBuf::from("/home/garrett/test-archive");
+    let mut archive = Archive::create(&path)?;
 
     let mut entry = ArchiveEntry::new();
-    let handle = archive.write(&mut [0u8; 8].as_ref()).unwrap();
-    entry.metadata.insert(String::from("tag"), vec![0u8; 4]);
+    let handle = archive.write(&mut b"Garrett".as_ref())?;
+    entry.metadata.insert(String::from("Tag"), b"Meta".to_vec());
     entry.data = Some(handle);
 
     archive.insert("null", entry);
+    archive.commit()?;
+
+    Ok(())
 }
