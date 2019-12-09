47e63720f8a8d60d0e47c40852dbfc1a987eb2c3
Wren Powell <wrentpowell@gmail.com>
2019-12-09 22:58:58 +0000

    Add code for reading/writing the superblock

--- src/object/block.rs
+++ src/object/block.rs
@@ -14,13 +14,20 @@
  * limitations under the License.
  */
 
+use std::fs::File;
+use std::io::{Read, Seek, SeekFrom, Write};
+use std::mem::size_of;
+
+use rmp_serde::{from_read, to_vec};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+
+use crate::error::Result;
 
 use super::compression::Compression;
 use super::encryption::Encryption;
 
-/// The offset of the superblock from the start of the file.
+/// The offset of the primary superblock from the start of the file.
 const SUPERBLOCK_OFFSET: u64 = 0;
 
 /// The offset of the backup superblock from the start of the file.
@@ -68,4 +75,56 @@ pub struct SuperBlock {
 
     /// The extent which stores the repository's header.
     pub header: Extent,
+}
+
+impl SuperBlock {
+    /// Read the superblock from the given `file` stored at the given `offset`.
+    ///
+    /// # Errors
+    /// - `Error::Io`: An I/O error occurred.
+    /// - `Error::Deserialize`: The superblock could not be deserialized.
+    fn read_at(file: &mut File, offset: u64) -> Result<Self> {
+        file.seek(SeekFrom::Start(offset))?;
+
+        // Get the size of the superblock.
+        let mut size_buffer = [0u8; size_of::<u32>()];
+        file.read_exact(&mut size_buffer)?;
+        let superblock_size = u32::from_be_bytes(size_buffer) as u64;
+
+        // Deserialize the superblock.
+        Ok(from_read(file.take(superblock_size))?)
+    }
+
+    /// Write the superblock to the given `file` at the given `offset`.
+    ///
+    /// # Errors
+    /// - `Error::Io`: An I/O error occurred.
+    /// - `Error::Serialize`: The superblock could not be serialized.
+    fn write_at(&self, file: &mut File, offset: u64) -> Result<()> {
+        file.seek(SeekFrom::Start(offset))?;
+
+        // Serialize the superblock.
+        let superblock = to_vec(&self)?;
+        let superblock_size = superblock.len();
+
+        // Write the superblock size and the superblock itself.
+        file.write_all(&superblock_size.to_be_bytes())?;
+        file.write_all(&superblock)?;
+
+        Ok(())
+    }
+
+    /// Read the superblock from the given `file` or the backup superblock if it is corrupt.
+    pub fn read(file: &mut File) -> Result<Self> {
+        Self::read_at(file, SUPERBLOCK_OFFSET)
+            .or_else(|_| Self::read_at(file, SUPERBLOCK_BACKUP_OFFSET))
+    }
+
+    /// Write this superblock to the given `file` twice, a primary and a backup.
+    pub fn write(&self, file: &mut File) -> Result<()> {
+        self.write_at(file, SUPERBLOCK_OFFSET)?;
+        self.write_at(file, SUPERBLOCK_BACKUP_OFFSET)?;
+
+        Ok(())
+    }
 }
