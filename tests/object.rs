71e0c5cf341a39ad146ea474057a79920ba1e56e
Garrett Powell <garrett@gpowell.net>
2019-12-13 17:48:13 +0000

    Refactor tests with helper methods

--- tests/object.rs
+++ tests/object.rs
@@ -31,15 +31,24 @@ fn random_bytes(size: usize) -> Vec<u8> {
     buffer
 }
 
-/// The size of a small test data buffer.
-///
-/// This is smaller than the archive block size.
-const SMALL_DATA_SIZE: usize = 2048;
+/// Insert random data into the `archive` with the given `key`.
+fn insert_data(key: &str, archive: &mut ObjectArchive<String>) -> io::Result<Vec<u8>> {
+    let data = random_bytes(DATA_SIZE);
+    let object = archive.write(data.as_slice())?;
+    archive.insert(key.to_string(), object);
+    Ok(data)
+}
 
-/// The size of a large test data buffer.
+/// Retrieve the data in the `archive` associated with the given `key`.
+fn read_data(key: &str, archive: &ObjectArchive<String>) -> io::Result<Vec<u8>> {
+    let object = archive.get(&key.to_string()).unwrap();
+    archive.read_all(&object)
+}
+
+/// The size of a test data buffer.
 ///
 /// This is considerably larger than the archive block size, but not an exact multiple of it.
-const LARGE_DATA_SIZE: usize = (1024 * 1024 * 4) + 200;
+const DATA_SIZE: usize = (1024 * 1024 * 4) + 200;
 
 /// The archive config to use for testing.
 const ARCHIVE_CONFIG: ArchiveConfig = ArchiveConfig {
@@ -52,43 +61,19 @@ const ARCHIVE_CONFIG: ArchiveConfig = ArchiveConfig {
 // TODO: Use macros to generate similar tests.
 
 #[test]
-fn small_object_is_persisted() -> io::Result<()> {
+fn object_is_persisted() -> io::Result<()> {
     let temp_dir = tempdir()?;
     let archive_path = temp_dir.path().join("archive");
     let mut archive = ObjectArchive::create(archive_path.as_path(), ARCHIVE_CONFIG, None)?;
 
-    let expected_data = random_bytes(SMALL_DATA_SIZE);
-    let object = archive.write(expected_data.as_slice())?;
-    archive.insert("Test".to_string(), object);
+    let expected_data = insert_data("Test", &mut archive)?;
 
     archive.commit()?;
     drop(archive);
 
     let archive = ObjectArchive::open(archive_path.as_path(), None)?;
-    let object = archive.get(&"Test".to_string()).unwrap();
-    let actual_data = archive.read_all(&object)?;
 
-    assert_eq!(expected_data, actual_data);
-
-    Ok(())
-}
-
-#[test]
-fn large_object_is_persisted() -> io::Result<()> {
-    let temp_dir = tempdir()?;
-    let archive_path = temp_dir.path().join("archive");
-    let mut archive = ObjectArchive::create(archive_path.as_path(), ARCHIVE_CONFIG, None)?;
-
-    let expected_data = random_bytes(LARGE_DATA_SIZE);
-    let object = archive.write(expected_data.as_slice())?;
-    archive.insert("Test".to_string(), object);
-
-    archive.commit()?;
-    drop(archive);
-
-    let archive = ObjectArchive::open(archive_path.as_path(), None)?;
-    let object = archive.get(&"Test".to_string()).unwrap();
-    let actual_data = archive.read_all(&object)?;
+    let actual_data = read_data("Test", &archive)?;
 
     assert_eq!(expected_data, actual_data);
 
@@ -101,31 +86,18 @@ fn multiple_objects_are_persisted() -> io::Result<()> {
     let archive_path = temp_dir.path().join("archive");
     let mut archive = ObjectArchive::create(archive_path.as_path(), ARCHIVE_CONFIG, None)?;
 
-    let expected_data1 = random_bytes(LARGE_DATA_SIZE);
-    let object1 = archive.write(expected_data1.as_slice())?;
-    archive.insert("Test1".to_string(), object1);
-
-    let expected_data2 = random_bytes(SMALL_DATA_SIZE);
-    let object2 = archive.write(expected_data2.as_slice())?;
-    archive.insert("Test2".to_string(), object2);
-
-    let expected_data3 = random_bytes(LARGE_DATA_SIZE);
-    let object3 = archive.write(expected_data3.as_slice())?;
-    archive.insert("Test3".to_string(), object3);
+    let expected_data1 = insert_data("Test1", &mut archive)?;
+    let expected_data2 = insert_data("Test2", &mut archive)?;
+    let expected_data3 = insert_data("Test3", &mut archive)?;
 
     archive.commit()?;
     drop(archive);
 
     let archive = ObjectArchive::open(archive_path.as_path(), None)?;
 
-    let object1 = archive.get(&"Test1".to_string()).unwrap();
-    let actual_data1 = archive.read_all(&object1)?;
-
-    let object2 = archive.get(&"Test2".to_string()).unwrap();
-    let actual_data2 = archive.read_all(&object2)?;
-
-    let object3 = archive.get(&"Test3".to_string()).unwrap();
-    let actual_data3 = archive.read_all(&object3)?;
+    let actual_data1 = read_data("Test1", &archive)?;
+    let actual_data2 = read_data("Test2", &archive)?;
+    let actual_data3 = read_data("Test3", &archive)?;
 
     assert_eq!(expected_data1, actual_data1);
     assert_eq!(expected_data2, actual_data2);
@@ -140,23 +112,19 @@ fn removed_objects_are_overwritten() -> io::Result<()> {
     let archive_path = temp_dir.path().join("archive");
     let mut archive = ObjectArchive::create(archive_path.as_path(), ARCHIVE_CONFIG, None)?;
 
-    let expected_data1 = random_bytes(LARGE_DATA_SIZE);
-    let object1 = archive.write(expected_data1.as_slice())?;
-    archive.insert("Test1".to_string(), object1);
+    insert_data("Test1", &mut archive)?;
 
     archive.commit()?;
     archive.remove(&"Test1".to_string());
     archive.commit()?;
 
-    let expected_data2 = random_bytes(LARGE_DATA_SIZE);
-    let object2 = archive.write(expected_data2.as_slice())?;
-    archive.insert("Test2".to_string(), object2);
+    insert_data("Test2", &mut archive)?;
 
     archive.commit()?;
     drop(archive);
 
     let archive_size = metadata(archive_path)?.len();
-    assert!(archive_size < (LARGE_DATA_SIZE * 2) as u64);
+    assert!(archive_size < (DATA_SIZE * 2) as u64);
 
     Ok(())
 }
@@ -167,10 +135,8 @@ fn uncommitted_changes_are_not_saved() -> io::Result<()> {
     let archive_path = temp_dir.path().join("archive");
     let mut archive = ObjectArchive::create(archive_path.as_path(), Default::default(), None)?;
 
-    let expected_data = b"This is data.";
-    let object = archive.write(&mut expected_data.as_ref())?;
+    insert_data("Test", &mut archive)?;
 
-    archive.insert("Test".to_string(), object);
     drop(archive);
 
     let archive = ObjectArchive::open(archive_path.as_path(), None)?;
