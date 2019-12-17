2246816f4c914c06c3b01d4ab3bac57aaf4bb445
Wren Powell <wrentpowell@gmail.com>
2019-12-17 12:38:25 -0500

    Fix problem building on Windows

--- src/file/platform.rs
+++ src/file/platform.rs
@@ -16,33 +16,27 @@
 
 use std::collections::HashMap;
 use std::ffi::OsString;
-use std::fs::{set_permissions, Metadata, Permissions};
+use std::fs::{Metadata, set_permissions};
 use std::io;
 #[cfg(unix)]
-use std::os::unix::fs::{symlink, PermissionsExt};
+use std::os::unix::fs::{PermissionsExt, symlink};
 #[cfg(windows)]
 use std::os::windows::fs::symlink_file;
 use std::path::Path;
 
-/// Gets the file mode from the given file `metadata`.
-///
-/// This will return `None` if POSIX permissions are not supported by the given platform.
+/// Get the file mode from the given file `metadata`.
 #[cfg(unix)]
 pub fn file_mode(metadata: &Metadata) -> Option<u32> {
     Some(metadata.permissions().mode())
 }
 
-/// Gets the file mode from the given file `metadata`.
-///
-/// This will return `None` if POSIX permissions are not supported by the given platform.
+/// Return `None` because POSIX permissions are not supported by this platform.
 #[cfg(windows)]
 pub fn file_mode(metadata: &Metadata) -> Option<u32> {
     None
 }
 
 /// Set the given file `mode` on the given `file`.
-///
-/// This does nothing if POSIX permissions are not supported by the current platform.
 #[cfg(unix)]
 pub fn set_file_mode(file: &Path, mode: u32) -> io::Result<()> {
     set_permissions(file, Permissions::from_mode(mode))?;
@@ -50,55 +44,55 @@ pub fn set_file_mode(file: &Path, mode: u32) -> io::Result<()> {
     Ok(())
 }
 
-/// Set the given file `mode` on the given `file`.
-///
-/// This does nothing if POSIX permissions are not supported by the current platform.
+/// Do nothing because POSIX permissions are not supported by this platform.
 #[cfg(windows)]
 pub fn set_file_mode(file: &Path, mode: u32) -> io::Result<()> {
     Ok(())
 }
 
-/// Returns a map of the extended attributes of the given `file`.
-///
-/// The map will be empty if extended attributes are not supported by the current platform.
+/// Return a map of the extended attributes of the given `file`.
+#[cfg(unix)]
 pub fn extended_attrs(file: &Path) -> io::Result<HashMap<OsString, Vec<u8>>> {
     let mut attributes = HashMap::new();
 
-    if xattr::SUPPORTED_PLATFORM {
-        for attr_name in xattr::list(file)? {
-            if let Some(attr_value) = xattr::get(file, &attr_name)? {
-                attributes.insert(attr_name, attr_value);
-            }
+    for attr_name in xattr::list(file)? {
+        if let Some(attr_value) = xattr::get(file, &attr_name)? {
+            attributes.insert(attr_name, attr_value);
         }
     }
 
     Ok(attributes)
 }
 
+/// Return an empty map because extended attributes are not supported by this platform.
+#[cfg(windows)]
+pub fn extended_attrs(file: &Path) -> io::Result<HashMap<OsString, Vec<u8>>> {
+    Ok(HashMap::new())
+}
+
 /// Sets the given `attributes` on the given `file`.
-///
-/// This does nothing if extended attributes are not supported by the current platform.
+#[cfg(unix)]
 pub fn set_extended_attrs(file: &Path, attributes: HashMap<OsString, Vec<u8>>) -> io::Result<()> {
-    if xattr::SUPPORTED_PLATFORM {
-        for (attr_name, attr_value) in attributes.iter() {
-            xattr::set(file, attr_name, attr_value)?;
-        }
+    for (attr_name, attr_value) in attributes.iter() {
+        xattr::set(file, attr_name, attr_value)?;
     }
 
     Ok(())
 }
 
+/// Do nothing because extended attributes are not supported by this platform.
+#[cfg(windows)]
+pub fn set_extended_attrs(file: &Path, attributes: HashMap<OsString, Vec<u8>>) -> io::Result<()> {
+    Ok(())
+}
+
 /// Create a symbolic `link` to a given `target` file.
-///
-/// On Windows, this attempts to create a file link (not a directory link).
 #[cfg(unix)]
 pub fn soft_link(link: &Path, target: &Path) -> io::Result<()> {
     symlink(target, link)
 }
 
-/// Create a symbolic `link` to a given `target` file.
-///
-/// On Windows, this attempts to create a file link (not a directory link).
+/// Create a symbolic file `link` (not a directory link) to a given `target` file.
 #[cfg(windows)]
 pub fn soft_link(link: &Path, target: &Path) -> io::Result<()> {
     symlink_file(target, link)
