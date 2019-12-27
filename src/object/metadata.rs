77d66cdf607ac785037c1775ee5f57cf5a54088b
Wren Powell <wrentpowell@gmail.com>
2019-12-27 18:47:02 -0500

    Initial version of data store interface

---
+++ src/object/metadata.rs
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 Wren Powell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/// Metadata for a repository.
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct RepositoryMetadata<ID> {
+    /// The unique ID of this repository.
+    pub id: Uuid,
+
+    /// The number of bits that define a chunk boundary.
+    ///
+    /// The average size of a chunk will be 2^`chunker_bits` bytes.
+    pub chunker_bits: u32,
+
+    /// The compression method being used in this repository.
+    pub compression: Compression,
+
+    /// The encryption method being used in this repository.
+    pub encryption: Encryption,
+
+    /// The hash algorithm used for computing object checksums.
+    pub hash_algorithm: HashAlgorithm,
+
+    /// The ID of the chunk which stores the repository's header.
+    pub header: ID,
+
+    /// The size of the header in bytes.
+    pub header_size: u32,
+}
