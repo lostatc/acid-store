5244f187be6984903ad3be59544239f4153bca5a
Wren Powell <wrentpowell@gmail.com>
2019-12-30 16:08:53 -0500

    Update crate-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -14,36 +14,26 @@
  * limitations under the License.
  */
 
-// TODO: Rewrite crate-level documentation.
-
-//! `disk-archive` is a library for creating high-performance file formats.
+//! `data-store` is a library for secure, deduplicated, transactional, and verifiable data storage.
 //!
-//! Many application file formats are built on ZIP archives or SQLite databases. ZIP archives are
-//! difficult to update in-place, requiring the entire archive to be unpacked and repacked, which
-//! can be expensive for large archives. SQLite databases aren't well-suited for storing large
-//! binary blobs, which can be problematic for applications where this is a requirement.
-//!
-//! This crate provides a custom file format that is designed for storing large binary blobs. It's
-//! meant to be used as an alternative to ZIP archives or SQLite databases for creating
-//! high-performance file formats. The archive format supports atomic transactions and in-place
-//! updates like SQLite while being more suited for storing large blobs.
-//!
-//! Archives created with this crate support the following features:
+//! This crate provides high-level abstractions for data storage over a number of storage backends.
+//! Storage backends are easy to implement, and this library builds on top of them to provide the
+//! following features:
 //! - Content-defined block-level deduplication
-//! - Transparent compression
 //! - Transparent encryption
-//! - Integrity checking of data
+//! - Transparent compression
+//! - Automatic checksumming and integrity checking
 //! - Atomicity, consistency, isolation, and durability (ACID)
 //!
-//! For applications which only need to store small (<100KiB) blobs, a SQLite database may be a
-//! better choice. For applications which require an open/transparent format, consider using a ZIP
-//! archive.
+//! This library currently provides two abstractions for data storage:
+//! - `ObjectRepository` is an object store which maps keys to binary blobs.
+//! - `FileRepository` is a file archive like ZIP or TAR which supports modification times, POSIX
+//! file permissions, extended attributes, and symbolic links.
 //!
-//! This crate provides two abstractions over the archive format:
-//! - `FileArchive` is a file archive like ZIP or TAR which supports modification times, POSIX file
-//! permissions, extended attributes, and symbolic links.
-//! - `ObjectArchive` is an object store which represents data as a flat mapping of keys to binary
-//! blobs instead of a hierarchy of files.
+//! A repository stores its data in a `DataStore`, which is a small trait that can be implemented to
+//! create new storage backends. The following data stores are provided out of the box:
+//! - `DirectoryStore` stores data in a directory in the local file system.
+//! - `MemoryStore` stores data in memory.
 
 #![allow(dead_code)]
 
