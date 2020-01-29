4e55364fbab1906ab69811bbc33e96b2b51d8b06
Garrett Powell <garrett@gpowell.net>
2020-01-29 13:09:11 -0500

    Add Redis data store

---
+++ src/store/redis.rs
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2019 Garrett Powell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#![cfg(feature = "store-redis")]
+
+use redis::{Client, Commands, Connection, IntoConnectionInfo, RedisError};
+use uuid::Uuid;
+
+use crate::store::DataStore;
+
+/// A UUID which acts as the version ID of the store format.
+const CURRENT_VERSION: &str = "b733bd82-4206-11ea-a3dc-7354076bdaf9";
+
+pub struct RedisStore {
+    connection: Connection,
+}
+
+impl RedisStore {
+    /// Create a new `RedisStore` on the Redis server accessible from `connection`.
+    ///
+    /// # Errors
+    /// - `Error::AlreadyExists`: There is already a `RedisStore` on the server.
+    /// - `Error::Store`: There was a Redis error.
+    pub fn create(connection: impl IntoConnectionInfo) -> crate::Result<Self> {
+        let client = Client::open(connection).map_err(anyhow::Error::from)?;
+        let mut connection = client.get_connection().map_err(anyhow::Error::from)?;
+
+        let version_response: Option<String> =
+            connection.get("version").map_err(anyhow::Error::from)?;
+
+        if let Some(version) = version_response {
+            if version == *CURRENT_VERSION {
+                return Err(crate::Error::AlreadyExists);
+            }
+        }
+
+        connection
+            .set("version", CURRENT_VERSION)
+            .map_err(anyhow::Error::from)?;
+
+        Ok(Self { connection })
+    }
+
+    /// Open the existing `RedisStore` on the Redis server accessible from `connection`.
+    ///
+    /// # Errors
+    /// - `Error::UnsupportedFormat`: There is not a `RedisStore` on the server or it is an
+    /// unsupported format.
+    /// - `Error::Store`: There was a Redis error.
+    pub fn open(connection: impl IntoConnectionInfo) -> crate::Result<Self> {
+        let client = Client::open(connection).map_err(anyhow::Error::from)?;
+        let mut connection = client.get_connection().map_err(anyhow::Error::from)?;
+
+        let version_response: Option<String> =
+            connection.get("version").map_err(anyhow::Error::from)?;
+
+        match version_response {
+            Some(version) if version == *CURRENT_VERSION => Ok(Self { connection }),
+            Some(_) => Err(crate::Error::UnsupportedFormat),
+            None => Err(crate::Error::UnsupportedFormat),
+        }
+    }
+
+    /// Destroy the `RedisStore` on the Redis server accessible from `connection`.
+    ///
+    /// **This flushes all data from the server.** Once this method returns `Ok`, `create` can be
+    /// called to create a new `RedisStore` on the server.
+    ///
+    /// # Errors
+    /// - `Error::Store`: There was a Redis error.
+    pub fn destroy(mut self) -> crate::Result<()> {
+        redis::cmd("FLUSHDB")
+            .query(&mut self.connection)
+            .map_err(anyhow::Error::from)?;
+        Ok(())
+    }
+}
+
+impl DataStore for RedisStore {
+    type Error = RedisError;
+
+    fn write_block(&mut self, id: Uuid, data: &[u8]) -> Result<(), Self::Error> {
+        let key_id = id.to_hyphenated().to_string();
+        self.connection.set(format!("block:{}", key_id), data)?;
+        Ok(())
+    }
+
+    fn read_block(&mut self, id: Uuid) -> Result<Option<Vec<u8>>, Self::Error> {
+        let key_id = id.to_hyphenated().to_string();
+        self.connection.get(format!("block:{}", key_id))
+    }
+
+    fn remove_block(&mut self, id: Uuid) -> Result<(), Self::Error> {
+        let key_id = id.to_hyphenated().to_string();
+        self.connection.del(format!("block:{}", key_id))?;
+        Ok(())
+    }
+
+    fn list_blocks(&mut self) -> Result<Vec<Uuid>, Self::Error> {
+        let blocks = self
+            .connection
+            .keys::<_, Vec<String>>("block:*")?
+            .iter()
+            .map(|key| {
+                let uuid = key.trim_start_matches("block:");
+                Uuid::parse_str(uuid).expect("Could not parse UUID.")
+            })
+            .collect();
+        Ok(blocks)
+    }
+}
