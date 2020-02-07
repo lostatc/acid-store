3fe680ddeb5f94976257c46fff0a3bb7932e59a8
Wren Powell <wrentpowell@gmail.com>
2020-02-07 14:50:00 -0500

    Add tests for version repository

---
+++ tests/version_repository.rs
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2019-2020 Wren Powell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#![cfg(feature = "repo-version")]
+
+use std::io::{Read, Write};
+
+use matches::assert_matches;
+
+use acid_store::repo::VersionRepository;
+use acid_store::store::MemoryStore;
+use common::{assert_contains_all, random_buffer, ARCHIVE_CONFIG, PASSWORD};
+
+mod common;
+
+fn create_repo() -> acid_store::Result<VersionRepository<String, MemoryStore>> {
+    VersionRepository::create_repo(MemoryStore::new(), ARCHIVE_CONFIG, Some(PASSWORD))
+}
+
+#[test]
+fn get_version() -> anyhow::Result<()> {
+    let mut repository = create_repo()?;
+
+    // Add a new object and write data to it.
+    let expected_data = random_buffer();
+    let mut object = repository.insert("Key".into())?;
+    object.write_all(expected_data.as_slice())?;
+    object.flush()?;
+    drop(object);
+
+    // Create a new version of the object.
+    let version = repository.create_version("Key".into())?;
+
+    // Read the new version.
+    let mut object = repository
+        .get_version("Key", version.id())
+        .ok_or(acid_store::Error::NotFound)?;
+    let mut actual_data = Vec::new();
+    object.read_to_end(&mut actual_data)?;
+    drop(object);
+
+    assert_eq!(actual_data, expected_data);
+
+    Ok(())
+}
+
+#[test]
+fn list_versions() -> anyhow::Result<()> {
+    let mut repository = create_repo()?;
+
+    repository.insert("Key".into())?;
+    let version1 = repository.create_version("Key".into())?;
+    let version2 = repository.create_version("Key".into())?;
+    let version3 = repository.create_version("Key".into())?;
+
+    let expected = vec![version1, version2, version3];
+    let actual = repository.list_versions("Key")?;
+
+    assert_contains_all(actual, expected);
+
+    Ok(())
+}
+
+#[test]
+fn remove_version() -> anyhow::Result<()> {
+    let mut repository = create_repo()?;
+
+    repository.insert("Key".into())?;
+    let version = repository.create_version("Key".into())?;
+    repository.remove_version("Key", version.id())?;
+
+    assert!(repository.get_version("Key", version.id()).is_none());
+    Ok(())
+}
+
+#[test]
+fn remove_and_list_versions() -> anyhow::Result<()> {
+    let mut repository = create_repo()?;
+
+    repository.insert("Key".into())?;
+    let version1 = repository.create_version("Key".into())?;
+    let version2 = repository.create_version("Key".into())?;
+    let version3 = repository.create_version("Key".into())?;
+    repository.remove_version("Key", version2.id())?;
+
+    let expected = vec![version1, version3];
+    let actual = repository.list_versions("Key")?;
+
+    assert_contains_all(actual, expected);
+
+    Ok(())
+}
+
+#[test]
+fn versioning_nonexistent_key_errs() -> anyhow::Result<()> {
+    let mut repository = create_repo()?;
+    assert_matches!(
+        repository.create_version("Key".into()),
+        Err(acid_store::Error::NotFound)
+    );
+    assert_matches!(
+        repository.remove_version("Key", 1),
+        Err(acid_store::Error::NotFound)
+    );
+    assert_matches!(
+        repository.list_versions("Key"),
+        Err(acid_store::Error::NotFound)
+    );
+    Ok(())
+}
+
+#[test]
+fn removing_key_removes_versions() -> anyhow::Result<()> {
+    let mut repository = create_repo()?;
+
+    repository.insert("Key".into())?;
+    let version = repository.create_version("Key".into())?;
+    repository.remove("Key")?;
+
+    assert!(repository.get_version("Key", version.id()).is_none());
+    assert_matches!(
+        repository.list_versions("Key"),
+        Err(acid_store::Error::NotFound)
+    );
+    Ok(())
+}
+
+#[test]
+fn modifying_object_doesnt_modify_versions() -> anyhow::Result<()> {
+    let mut repository = create_repo()?;
+
+    repository.insert("Key".into())?;
+    let version = repository.create_version("Key".into())?;
+
+    let mut object = repository.get("Key").unwrap();
+    object.write_all(random_buffer().as_slice())?;
+    object.flush()?;
+    drop(object);
+
+    let object = repository.get_version("Key", version.id()).unwrap();
+    assert_eq!(object.size(), 0);
+
+    Ok(())
+}
