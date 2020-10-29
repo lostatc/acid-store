3f30eb4563e21713d41345843686921cd0c0a756
Wren Powell <wrentpowell@gmail.com>
2020-10-29 11:38:19 -0400

    Add baseline IO benchmarks

--- benches/io.rs
+++ benches/io.rs
@@ -14,20 +14,25 @@
  * limitations under the License.
  */
 
-use std::fs::remove_dir_all;
-use std::io::{Read, Seek, SeekFrom, Write};
+use std::cell::RefCell;
+use std::fs::{remove_dir_all, File, OpenOptions as FileOpenOptions};
+use std::io::{Read, Write};
 use std::path::Path;
+use std::time::Duration;
 
-use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion, Throughput};
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion, Throughput};
+use hex_literal::hex;
 use rand::rngs::SmallRng;
-use rand::seq::SliceRandom;
 use rand::{RngCore, SeedableRng};
 use tempfile::tempdir;
+use uuid::Uuid;
 
 use acid_store::repo::key::KeyRepo;
 use acid_store::repo::object::ObjectRepo;
 use acid_store::repo::{Chunking, Encryption, OpenOptions};
 use acid_store::store::DirectoryStore;
+
+const TEST_OBJECT_UUID: Uuid = Uuid::from_bytes(hex!("375e9f00 d476 4f2c a5db 1aec51c0a240"));
 
 /// Return a buffer containing `size` random bytes for testing purposes.
 pub fn random_bytes(size: usize) -> Vec<u8> {
@@ -174,32 +179,105 @@ pub fn insert_object_and_write(criterion: &mut Criterion) {
     }
 }
 
+pub fn write_baseline(criterion: &mut Criterion) {
+    let tmp_dir = tempdir().unwrap();
+    let file_path = tmp_dir.as_ref().join("test");
+
+    let mut group = criterion.benchmark_group("Baseline write");
+
+    let object_size = bytesize::mib(1u64);
+
+    group.throughput(Throughput::Bytes(object_size));
+    group.sample_size(50);
+    group.measurement_time(Duration::from_secs(30));
+
+    group.bench_function(bytesize::to_string(object_size, true), |bencher| {
+        bencher.iter_batched(
+            || {
+                let file = File::create(&file_path).unwrap();
+                let data = random_bytes(object_size as usize);
+                (file, data)
+            },
+            |(mut file, data)| {
+                file.write_all(data.as_slice()).unwrap();
+                file.flush().unwrap();
+            },
+            BatchSize::PerIteration,
+        );
+    });
+}
+
 pub fn write_object(criterion: &mut Criterion) {
     let tmp_dir = tempdir().unwrap();
 
     let mut group = criterion.benchmark_group("Write to an object");
 
-    let object_size = bytesize::kib(100u64);
+    let object_size = bytesize::mib(1u64);
 
-    for (mut repo, name) in test_configs(tmp_dir.path()) {
+    for (repo, name) in test_configs(tmp_dir.path()) {
         group.throughput(Throughput::Bytes(object_size));
+        group.sample_size(50);
+        group.measurement_time(Duration::from_secs(30));
 
-        group.bench_function(
+        group.bench_with_input(
             format!("{}, {}", bytesize::to_string(object_size, true), name),
-            |bencher| {
+            &RefCell::new(repo),
+            |bencher, repo_cell| {
                 bencher.iter_batched(
-                    || random_bytes(object_size as usize),
+                    || {
+                        let mut repo = repo_cell.borrow_mut();
+                        repo.add_managed(TEST_OBJECT_UUID);
+                        random_bytes(object_size as usize)
+                    },
                     |data| {
-                        let mut handle = repo.add_unmanaged();
-                        let mut object = repo.unmanaged_object_mut(&mut handle).unwrap();
+                        // Write data to the object.
+                        let mut repo = repo_cell.borrow_mut();
+                        let mut object = repo.managed_object_mut(TEST_OBJECT_UUID).unwrap();
                         object.write_all(data.as_slice()).unwrap();
                         object.flush().unwrap();
                     },
-                    BatchSize::SmallInput,
+                    BatchSize::PerIteration,
                 );
             },
         );
     }
+}
+
+pub fn read_baseline(criterion: &mut Criterion) {
+    let tmp_dir = tempdir().unwrap();
+    let file_path = tmp_dir.as_ref().join("file");
+
+    let mut group = criterion.benchmark_group("Baseline read");
+
+    let object_size = bytesize::mib(1u64);
+
+    group.throughput(Throughput::Bytes(object_size));
+    group.sample_size(50);
+    group.measurement_time(Duration::from_secs(30));
+
+    group.bench_function(bytesize::to_string(object_size, true), |bencher| {
+        bencher.iter_batched(
+            || {
+                let mut file = FileOpenOptions::new()
+                    .create(true)
+                    .truncate(true)
+                    .write(true)
+                    .read(true)
+                    .open(&file_path)
+                    .unwrap();
+                file.write_all(random_bytes(object_size as usize).as_slice())
+                    .unwrap();
+                file.flush().unwrap();
+                file
+            },
+            |mut file| {
+                let mut buffer = Vec::new();
+                file.read_to_end(&mut buffer).unwrap();
+                buffer
+            },
+            BatchSize::PerIteration,
+        );
+    });
 }
 
 pub fn read_object(criterion: &mut Criterion) {
@@ -207,83 +285,43 @@ pub fn read_object(criterion: &mut Criterion) {
 
     let mut group = criterion.benchmark_group("Read from an object");
 
-    let object_size = bytesize::kib(100u64);
-    let num_objects = 100;
+    let object_size = bytesize::mib(1u64);
 
-    for (mut repo, name) in test_configs(tmp_dir.path()) {
-        let mut handles = Vec::new();
-        // Generate a batch of random objects to read during the benchmark.
-        for _ in 0..num_objects {
-            let mut handle = repo.add_unmanaged();
-            let mut object = repo.unmanaged_object_mut(&mut handle).unwrap();
-            object
-                .write_all(random_bytes(object_size as usize).as_slice())
-                .unwrap();
-            object.flush().unwrap();
-            drop(object);
-            handles.push(handle);
-        }
-
+    for (repo, name) in test_configs(tmp_dir.path()) {
         group.throughput(Throughput::Bytes(object_size));
+        group.sample_size(50);
+        group.measurement_time(Duration::from_secs(30));
 
-        group.bench_function(
+        group.bench_with_input(
             format!("{}, {}", bytesize::to_string(object_size, true), name),
-            |bencher| {
+            &RefCell::new(repo),
+            |bencher, repo_cell| {
                 bencher.iter_batched(
                     || {
-                        // Select a random object to read.
-                        let mut rng = SmallRng::from_entropy();
-                        handles.choose(&mut rng).unwrap()
+                        // Write data to the object.
+                        let mut repo = repo_cell.borrow_mut();
+                        let mut object = repo.add_managed(TEST_OBJECT_UUID);
+                        object
+                            .write_all(random_bytes(object_size as usize).as_slice())
+                            .unwrap();
+                        object.flush().unwrap();
                     },
-                    |handle| {
+                    |_| {
+                        // Read data from the object.
+                        let repo = repo_cell.borrow_mut();
+                        let mut object = repo.managed_object(TEST_OBJECT_UUID).unwrap();
                         let mut buffer = Vec::new();
-                        let mut object = repo.unmanaged_object(&handle).unwrap();
                         object.read_to_end(&mut buffer).unwrap();
                         buffer
                     },
-                    BatchSize::SmallInput,
+                    BatchSize::PerIteration,
                 );
             },
         );
     }
 }
 
-pub fn write_read_object(criterion: &mut Criterion) {
-    let tmp_dir = tempdir().unwrap();
-
-    let mut group = criterion.benchmark_group("Write to and then read from an object");
-
-    let object_size = bytesize::kib(100u64);
-
-    for (mut repo, name) in test_configs(tmp_dir.path()) {
-        group.throughput(Throughput::Bytes(object_size));
-
-        group.bench_function(
-            format!("{}, {}", bytesize::to_string(object_size, true), name),
-            |bencher| {
-                bencher.iter_batched(
-                    || random_bytes(object_size as usize),
-                    |data| {
-                        // Write to the object.
-                        let mut handle = repo.add_unmanaged();
-                        let mut object = repo.unmanaged_object_mut(&mut handle).unwrap();
-                        object.write_all(data.as_slice()).unwrap();
-                        object.flush().unwrap();
-                        drop(object);
-
-                        // Read from the object.
-                        let mut buffer = Vec::new();
-                        let mut object = repo.unmanaged_object(&handle).unwrap();
-                        object.seek(SeekFrom::Start(0)).unwrap();
-                        object.read_to_end(black_box(&mut buffer)).unwrap();
-                    },
-                    BatchSize::SmallInput,
-                );
-            },
-        );
-    }
-}
-
-criterion_group!(throughput, read_object, write_object, write_read_object);
+criterion_group!(baseline, write_baseline, read_baseline);
+criterion_group!(throughput, read_object, write_object);
 criterion_group!(insert, insert_object, insert_object_and_write);
-criterion_main!(throughput);
+criterion_main!(baseline, throughput, insert);
