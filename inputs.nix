a702c103cfe03e7edd3a16b93d0f5b8fd11310a2
Wren Powell <wrentpowell@gmail.com>
2020-11-10 10:37:26 -0500

    Get std source code from Nix overlay

--- inputs.nix
+++ inputs.nix
@@ -1,9 +1,10 @@
 let
   mozilla_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   nixpkgs = import <nixpkgs> { overlays = [ mozilla_overlay ]; };
+  rust_stable = (nixpkgs.latest.rustChannels.stable.rust.override { extensions = [ "rust-src" ]; });
 in
   with nixpkgs; [
-    latest.rustChannels.stable.rust
+    rust_stable
     acl
     binutils
     gcc
