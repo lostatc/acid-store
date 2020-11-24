4bd29d42ec98fadfb789630b6ddec377c021aa62
Wren Powell <wrentpowell@gmail.com>
2020-11-24 16:11:46 -0500

    Remove Mozilla Nix overlay
    Using rustup to install the toolchain is generally preferable

--- inputs.nix
+++ inputs.nix
@@ -1,14 +1,10 @@
-let
-  mozilla_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-  nixpkgs = import <nixpkgs> { overlays = [ mozilla_overlay ]; };
-  rust_stable = (nixpkgs.latest.rustChannels.stable.rust.override { extensions = [ "rust-src" ]; });
-in
-  with nixpkgs; [
-    rust_stable
-    acl
-    binutils
-    gcc
-    gnumake
-    openssl
-    pkgconfig
-  ]
+with import <nixpkgs> {};
+
+with pkgs; [
+  acl
+  binutils
+  gcc
+  gnumake
+  openssl
+  pkgconfig
+]
