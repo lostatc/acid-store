9133bfc095a3b262b33c99cb74da0c348b6fd6aa
Wren Powell <wrentpowell@gmail.com>
2021-07-17 11:27:51 -0400

    Fix bench tests that did not compile

--- benches/io.rs
+++ benches/io.rs
@@ -14,195 +14,128 @@
  * limitations under the License.
  */
 
-use std::cell::RefCell;
-use std::io::{Read, Write};
+use std::io::{Read, Seek, SeekFrom, Write};
 use std::time::Duration;
 
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion, Throughput};
-use hex_literal::hex;
 use rand::rngs::SmallRng;
 use rand::{RngCore, SeedableRng};
-use uuid::Uuid;
 
 use acid_store::repo::key::KeyRepo;
-use acid_store::repo::object::ObjectRepo;
-use acid_store::repo::{Chunking, Encryption, OpenMode, OpenOptions, Packing};
+use acid_store::repo::{Chunking, Encryption, OpenMode, OpenOptions, Packing, RepoConfig};
 use acid_store::store::MemoryConfig;
+use once_cell::sync::Lazy;
 
-/// The ID of the managed object to write to for the bench test.
-const TEST_OBJECT_UUID: Uuid = Uuid::from_bytes(hex!("375e9f00 d476 4f2c a5db 1aec51c0a240"));
+/// The object key to use when performing I/O tests.
+const TEST_KEY: &str = "test";
 
-/// The number of bytes to write when a trivial amount of data must be written.
-const TRIVIAL_DATA_SIZE: usize = 16;
+/// The size of the data to read and write to objects.
+static OBJECT_SIZE: Lazy<u64> = Lazy::new(|| bytesize::mib(1u64));
 
 /// Return a buffer containing `size` random bytes for testing purposes.
-pub fn random_bytes(size: usize) -> Vec<u8> {
+fn random_bytes(size: usize) -> Vec<u8> {
     let mut rng = SmallRng::from_entropy();
     let mut buffer = vec![0u8; size];
     rng.fill_bytes(&mut buffer);
     buffer
 }
 
-fn create_repo_fixed() -> ObjectRepo {
-    OpenOptions::new()
-        .chunking(Chunking::fixed())
-        .mode(OpenMode::CreateNew)
-        .open(&MemoryConfig::new())
-        .unwrap()
-}
+fn open_repo(config: &RepoConfig) -> acid_store::Result<KeyRepo<String>> {
+    let mut options = OpenOptions::new();
 
-fn create_repo_zpaq() -> ObjectRepo {
-    OpenOptions::new()
-        .chunking(Chunking::zpaq())
-        .mode(OpenMode::CreateNew)
-        .open(&MemoryConfig::new())
-        .unwrap()
-}
+    options.config(config.clone()).mode(OpenMode::CreateNew);
 
-fn create_repo_fixed_encryption() -> ObjectRepo {
-    OpenOptions::new()
-        .chunking(Chunking::fixed())
-        .encryption(Encryption::XChaCha20Poly1305)
-        .packing(Packing::fixed())
-        .password(b"password")
-        .mode(OpenMode::CreateNew)
-        .open(&MemoryConfig::new())
-        .unwrap()
-}
-
-fn create_repo_zpaq_encryption() -> ObjectRepo {
-    OpenOptions::new()
-        .chunking(Chunking::zpaq())
-        .encryption(Encryption::XChaCha20Poly1305)
-        .packing(Packing::fixed())
-        .password(b"password")
-        .mode(OpenMode::CreateNew)
-        .open(&MemoryConfig::new())
-        .unwrap()
-}
-
-/// Return a list of repositories and test descriptions.
-fn test_configs() -> Vec<(ObjectRepo, String)> {
-    let fixed = (
-        create_repo_fixed(),
-        String::from("Chunking::Fixed, Packing::None, Encryption::None"),
-    );
-
-    let zpaq = (
-        create_repo_zpaq(),
-        String::from("Chunking::Zpaq, Packing::None, Encryption::None"),
-    );
-
-    let fixed_packing = (
-        create_repo_fixed_encryption(),
-        String::from("Chunking::Fixed, Packing::Fixed, Encryption::XChaCha20Poly1305"),
-    );
-
-    let zpaq_packing = (
-        create_repo_zpaq_encryption(),
-        String::from("Chunking::Zpaq, Packing::Fixed, Encryption::XChaCha20Poly1305"),
-    );
-
-    vec![fixed, zpaq, fixed_packing, zpaq_packing]
-}
-
-pub fn insert_object(criterion: &mut Criterion) {
-    let mut group = criterion.benchmark_group("Insert an object");
-
-    for num_objects in [100, 1_000, 10_000].iter() {
-        // Create a new repository.
-        let mut repo: KeyRepo<String> = OpenOptions::new()
-            .mode(OpenMode::CreateNew)
-            .open(&MemoryConfig::new())
-            .unwrap();
-
-        // Insert objects and write to them but don't commit them.
-        for i in 0..*num_objects {
-            let mut object = repo.insert(format!("Uncommitted object {}", i));
-            object
-                .write_all(random_bytes(TRIVIAL_DATA_SIZE).as_slice())
-                .unwrap();
-            object.flush().unwrap();
-        }
-
-        group.throughput(Throughput::Elements(1));
-
-        // Benchmark inserting a new object.
-        group.bench_function(
-            format!("with {} uncommitted objects", num_objects),
-            |bencher| {
-                bencher.iter(|| {
-                    repo.insert(String::from("Test"));
-                });
-            },
-        );
+    if config.encryption != Encryption::None {
+        options.password(b"Password");
     }
+
+    options.open(&MemoryConfig::new())
 }
 
-pub fn insert_object_and_write(criterion: &mut Criterion) {
-    let mut group = criterion.benchmark_group("Insert an object and write to it");
+pub struct TestSpec {
+    pub config: RepoConfig,
+    pub description: String,
+}
 
-    for num_objects in [100, 1_000, 10_000].iter() {
-        // Create a new repository.
-        let mut repo: KeyRepo<String> = OpenOptions::new()
-            .mode(OpenMode::CreateNew)
-            .open(&MemoryConfig::new())
-            .unwrap();
-
-        // Insert objects and write to them but don't commit them.
-        for i in 0..*num_objects {
-            let mut object = repo.insert(format!("Uncommitted object {}", i));
-            object
-                .write_all(random_bytes(TRIVIAL_DATA_SIZE).as_slice())
-                .unwrap();
-            object.flush().unwrap();
-        }
-
-        group.throughput(Throughput::Elements(1));
-
-        // Benchmark inserting a new object and writing to it.
-        group.bench_function(
-            format!("with {} uncommitted objects", num_objects),
-            |bencher| {
-                bencher.iter_batched(
-                    || random_bytes(TRIVIAL_DATA_SIZE),
-                    |data| {
-                        let mut object = repo.insert(String::from("Test"));
-                        object.write_all(data.as_slice()).unwrap();
-                        object.flush().unwrap();
-                    },
-                    BatchSize::SmallInput,
-                );
+static TEST_SPECS: Lazy<Vec<TestSpec>> = Lazy::new(|| {
+    vec![
+        TestSpec {
+            config: {
+                let mut config = RepoConfig::default();
+                config.chunking = Chunking::fixed();
+                config.packing = Packing::None;
+                config.encryption = Encryption::None;
+                config
             },
-        );
-    }
-}
+            description: String::from("Chunking::Fixed, Packing::None, Encryption::None"),
+        },
+        TestSpec {
+            config: {
+                let mut config = RepoConfig::default();
+                config.chunking = Chunking::zpaq();
+                config.packing = Packing::None;
+                config.encryption = Encryption::None;
+                config
+            },
+            description: String::from("Chunking::Zpaq, Packing::None, Encryption::None"),
+        },
+        TestSpec {
+            config: {
+                let mut config = RepoConfig::default();
+                config.chunking = Chunking::fixed();
+                config.packing = Packing::fixed();
+                config.encryption = Encryption::XChaCha20Poly1305;
+                config
+            },
+            description: String::from(
+                "Chunking::Fixed, Packing::Fixed, Encryption::XChaCha20Poly1305",
+            ),
+        },
+        TestSpec {
+            config: {
+                let mut config = RepoConfig::default();
+                config.chunking = Chunking::zpaq();
+                config.packing = Packing::fixed();
+                config.encryption = Encryption::XChaCha20Poly1305;
+                config
+            },
+            description: String::from(
+                "Chunking::Zpaq, Packing::Fixed, Encryption::XChaCha20Poly1305",
+            ),
+        },
+    ]
+});
 
 pub fn write_object(criterion: &mut Criterion) {
     let mut group = criterion.benchmark_group("Write to an object");
 
-    let object_size = bytesize::mib(1u64);
-
-    group.throughput(Throughput::Bytes(object_size));
+    group.throughput(Throughput::Bytes(*OBJECT_SIZE));
     group.measurement_time(Duration::from_secs(10));
     group.sample_size(50);
 
-    for (repo, name) in test_configs() {
+    for TestSpec {
+        config,
+        description,
+    } in &*TEST_SPECS
+    {
         group.bench_with_input(
-            format!("{}, {}", bytesize::to_string(object_size, true), name),
-            &RefCell::new(repo),
-            |bencher, repo_cell| {
+            format!(
+                "{}, {}",
+                bytesize::to_string(*OBJECT_SIZE, true),
+                description
+            ),
+            &config,
+            |bencher, config| {
                 bencher.iter_batched(
                     || {
-                        let mut repo = repo_cell.borrow_mut();
-                        repo.add_managed(TEST_OBJECT_UUID);
-                        random_bytes(object_size as usize)
+                        let mut repo = open_repo(config).unwrap();
+                        repo.insert(String::from(TEST_KEY));
+                        (repo, random_bytes(*OBJECT_SIZE as usize))
                     },
-                    |data| {
-                        let mut repo = repo_cell.borrow_mut();
-                        let mut object = repo.managed_object_mut(TEST_OBJECT_UUID).unwrap();
+                    |(repo, data)| {
+                        let mut object = repo.object(TEST_KEY).unwrap();
                         object.write_all(data.as_slice()).unwrap();
-                        object.flush().unwrap();
+                        object.commit().unwrap();
                     },
                     BatchSize::PerIteration,
                 );
@@ -214,30 +147,37 @@ pub fn write_object(criterion: &mut Criterion) {
 pub fn read_object(criterion: &mut Criterion) {
     let mut group = criterion.benchmark_group("Read from an object");
 
-    let object_size = bytesize::mib(1u64);
-
-    group.throughput(Throughput::Bytes(object_size));
+    group.throughput(Throughput::Bytes(*OBJECT_SIZE));
     group.measurement_time(Duration::from_secs(10));
     group.sample_size(50);
 
-    for (repo, name) in test_configs() {
+    for TestSpec {
+        config,
+        description,
+    } in &*TEST_SPECS
+    {
         group.bench_with_input(
-            format!("{}, {}", bytesize::to_string(object_size, true), name),
-            &RefCell::new(repo),
-            |bencher, repo_cell| {
+            format!(
+                "{}, {}",
+                bytesize::to_string(*OBJECT_SIZE, true),
+                description
+            ),
+            &config,
+            |bencher, config| {
                 bencher.iter_batched(
                     || {
                         // Write data to the object.
-                        let mut repo = repo_cell.borrow_mut();
-                        let mut object = repo.add_managed(TEST_OBJECT_UUID);
-                        let data = random_bytes(object_size as usize);
+                        let mut repo = open_repo(config).unwrap();
+                        let mut object = repo.insert(String::from(TEST_KEY));
+                        let data = random_bytes(*OBJECT_SIZE as usize);
                         object.write_all(data.as_slice()).unwrap();
-                        object.flush().unwrap();
+                        object.commit().unwrap();
+                        object.seek(SeekFrom::Start(0)).unwrap();
+                        repo
                     },
-                    |_| {
+                    |repo| {
                         // Read data from the object.
-                        let repo = repo_cell.borrow_mut();
-                        let mut object = repo.managed_object(TEST_OBJECT_UUID).unwrap();
+                        let mut object = repo.object(TEST_KEY).unwrap();
                         let mut buffer = Vec::new();
                         object.read_to_end(&mut buffer).unwrap();
                         buffer
@@ -250,5 +190,4 @@ pub fn read_object(criterion: &mut Criterion) {
 }
 
 criterion_group!(throughput, read_object, write_object);
-criterion_group!(insert, insert_object, insert_object_and_write);
 criterion_main!(throughput);
