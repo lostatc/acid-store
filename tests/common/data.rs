f957a9c822054dd03b3d57c86c2a26b4ef268ea3
Wren Powell <wrentpowell@gmail.com>
2021-07-24 09:20:37 -0400

    Update FileRepo tests

--- tests/common/data.rs
+++ tests/common/data.rs
@@ -17,6 +17,7 @@
 use rand::rngs::SmallRng;
 use rand::{Rng, RngCore, SeedableRng};
 use rstest::*;
+use tempfile::{tempdir, TempDir};
 
 /// The minimum size of test data buffers.
 const MIN_BUFFER_SIZE: usize = 2048;
@@ -57,4 +58,10 @@ pub fn smaller_buffer() -> Vec<u8> {
 pub fn larger_buffer() -> Vec<u8> {
     let mut rng = SmallRng::from_entropy();
     random_bytes(rng.gen_range(MAX_BUFFER_SIZE, MAX_BUFFER_SIZE * 2))
+}
+
+/// Return a temporary directory which is deleted when the test completes.
+#[fixture]
+pub fn temp_dir() -> TempDir {
+    tempdir().unwrap()
 }
