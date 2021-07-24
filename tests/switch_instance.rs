28032254e9eb6268dc1b62c1f6c1dabd9e798f86
Wren Powell <wrentpowell@gmail.com>
2021-07-24 15:33:17 -0400

    Update `SwitchInstance` tests

--- tests/switch_instance.rs
+++ tests/switch_instance.rs
@@ -17,64 +17,43 @@
 use std::io::Write;
 
 use acid_store::repo::key::KeyRepo;
-use acid_store::repo::{
-    Commit, OpenMode, OpenOptions, RepoConfig, SwitchInstance, DEFAULT_INSTANCE,
-};
-use acid_store::store::MemoryConfig;
+use acid_store::repo::{Commit, SwitchInstance, DEFAULT_INSTANCE};
 use acid_store::uuid::Uuid;
-use test_case::test_case;
 
-use common::random_buffer;
+use common::*;
 
 mod common;
 
-fn create_repo(
-    repo_config: RepoConfig,
-    store_config: &MemoryConfig,
-) -> acid_store::Result<KeyRepo<String>> {
-    OpenOptions::new()
-        .config(repo_config)
-        .password(b"Password")
-        .mode(OpenMode::CreateNew)
-        .open(store_config)
-}
+#[rstest]
+fn switching_instance_does_not_roll_back(repo_object: RepoObject) -> anyhow::Result<()> {
+    let RepoObject {
+        repo,
+        mut object,
+        key,
+    } = repo_object;
 
-#[test_case(common::FIXED_CONFIG.to_owned(); "with fixed-size chunking")]
-#[test_case(common::ENCODING_CONFIG.to_owned(); "with encryption and compression")]
-#[test_case(common::ZPAQ_CONFIG.to_owned(); "with ZPAQ chunking")]
-#[test_case(common::FIXED_PACKING_SMALL_CONFIG.to_owned(); "with a pack size smaller than the chunk size")]
-#[test_case(common::FIXED_PACKING_LARGE_CONFIG.to_owned(); "with a pack size larger than the chunk size")]
-#[test_case(common::ZPAQ_PACKING_CONFIG.to_owned(); "with packing and ZPAQ chunking")]
-fn switching_instance_does_not_roll_back(repo_config: RepoConfig) -> anyhow::Result<()> {
-    let store_config = MemoryConfig::new();
-    let mut repo = create_repo(repo_config, &store_config)?;
-
-    let mut object = repo.insert(String::from("test"));
-    object.write_all(random_buffer().as_slice())?;
+    object.write_all(b"test data")?;
     object.flush()?;
     drop(object);
 
     let repo: KeyRepo<String> = repo.switch_instance(Uuid::new_v4().into())?;
     let repo: KeyRepo<String> = repo.switch_instance(DEFAULT_INSTANCE)?;
 
-    assert!(repo.contains("test"));
-    assert!(repo.object("test").is_some());
+    assert_that!(repo.contains(&key)).is_true();
+    assert_that!(repo.object(&key)).is_some();
 
     Ok(())
 }
 
-#[test_case(common::FIXED_CONFIG.to_owned(); "with fixed-size chunking")]
-#[test_case(common::ENCODING_CONFIG.to_owned(); "with encryption and compression")]
-#[test_case(common::ZPAQ_CONFIG.to_owned(); "with ZPAQ chunking")]
-#[test_case(common::FIXED_PACKING_SMALL_CONFIG.to_owned(); "with a pack size smaller than the chunk size")]
-#[test_case(common::FIXED_PACKING_LARGE_CONFIG.to_owned(); "with a pack size larger than the chunk size")]
-#[test_case(common::ZPAQ_PACKING_CONFIG.to_owned(); "with packing and ZPAQ chunking")]
-fn switching_instance_does_not_commit(repo_config: RepoConfig) -> anyhow::Result<()> {
-    let store_config = MemoryConfig::new();
-    let mut repo = create_repo(repo_config, &store_config)?;
+#[rstest]
+fn switching_instance_does_not_commit(repo_object: RepoObject) -> anyhow::Result<()> {
+    let RepoObject {
+        repo,
+        mut object,
+        key,
+    } = repo_object;
 
-    let mut object = repo.insert(String::from("test"));
-    object.write_all(random_buffer().as_slice())?;
+    object.write_all(b"test data")?;
     object.flush()?;
     drop(object);
 
@@ -82,8 +61,8 @@ fn switching_instance_does_not_commit(repo_config: RepoConfig) -> anyhow::Result
     let mut repo: KeyRepo<String> = repo.switch_instance(DEFAULT_INSTANCE)?;
     repo.rollback()?;
 
-    assert!(!repo.contains("test"));
-    assert!(repo.object("test").is_none());
+    assert_that!(repo.contains(&key)).is_false();
+    assert_that!(repo.object(&key)).is_none();
 
     Ok(())
 }
